47dfad143ab0ae26e6b472fff58a5ceedad41382
RysevArtem <rysev.artem1986@gmail.com>
2023-03-17 17:03:42 +0300

    Added checking the content in the archive

--- test_zip_to_archive.py
+++ test_zip_to_archive.py
@@ -1,10 +1,23 @@
 from zipfile import ZipFile, ZIP_DEFLATED
 import os
-import pandas as pd
+import csv
+import PyPDF2
+import openpyxl
 
-csv_rows = len(pd.read_csv("examples/cities.csv"))
+xlsxfile = openpyxl.load_workbook('examples/sample3.xlsx')
+sheet = xlsxfile.active
+xlsx_rows = sheet.max_row
 
 
+with open("examples/cities.csv", 'r', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    csv_rows = 0
+    for row in csv_reader:
+        csv_rows += 1
+
+with open("examples/sample.pdf", "rb") as pdffile:
+    pdf_reader = PyPDF2.PdfReader(pdffile)
+    pdf_numpages = len(pdf_reader.pages)
 
 cities_size = os.path.getsize("examples/cities.csv")
 sample_size = os.path.getsize("examples/sample.pdf")
@@ -20,8 +33,15 @@ def test_check_files_in_archive():
         for row in cities:
             csv_rows_in_archive += 1
         myzip.write("examples/sample.pdf", "sample.pdf")
+        pdffile = myzip.open("sample.pdf")
+        pdf_reader = PyPDF2.PdfReader(pdffile)
+        pdf_numpages_in_archive = len(pdf_reader.pages)
         sample_size_in_archive = myzip.getinfo("sample.pdf").file_size
         myzip.write("examples/sample3.xlsx", "sample3.xlsx")
+        xlsxfile = myzip.open("sample3.xlsx")
+        xlsx_reader = openpyxl.load_workbook(xlsxfile)
+        sheet = xlsx_reader.active
+        xsls_rows_in_archive = sheet.max_row
         sample3_size_in_archive = myzip.getinfo("sample3.xlsx").file_size
         assert myzip.testzip() is None
 
@@ -30,3 +50,5 @@ def test_check_files_in_archive():
     assert sample_size == sample_size_in_archive, f"Размер файла в архиве не совпадает с тем, что был до архивирования"
     assert sample3_size == sample3_size_in_archive, f"Размер файла в архиве не совпадает с тем, что был до архивирования"
     assert csv_rows == csv_rows_in_archive, f"Количество строк csv файла в архиве не совпадает с изначальным кол-вом"
+    assert pdf_numpages == pdf_numpages_in_archive, f"Количество страниц pdf файла в архиве не совпадает с изначальным кол-вом"
+    assert xlsx_rows == xsls_rows_in_archive, f"Количество строк на активном листе файла xlsx в архиве не совпадает с изначальным кол-вом"
