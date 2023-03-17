90256be15115173026a57da606c3671a8fb0bdde
RysevArtem <rysev.artem1986@gmail.com>
2023-03-17 18:51:27 +0300

    Add functions

--- test_zip_to_archive.py
+++ test_zip_to_archive.py
@@ -4,27 +4,31 @@ import csv
 import PyPDF2
 import openpyxl
 
-xlsxfile = openpyxl.load_workbook('examples/sample3.xlsx')
-sheet = xlsxfile.active
-xlsx_rows = sheet.max_row
+
+def check_row_num_in_xlsx():
+    xlsxfile = openpyxl.load_workbook('examples/sample3.xlsx')
+    sheet = xlsxfile.active
+    xlsx_rows = sheet.max_row
+    return xlsx_rows
 
 
-with open("examples/cities.csv", 'r', newline='') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
-    csv_rows = 0
-    for row in csv_reader:
-        csv_rows += 1
-
-with open("examples/sample.pdf", "rb") as pdffile:
-    pdf_reader = PyPDF2.PdfReader(pdffile)
-    pdf_numpages = len(pdf_reader.pages)
-
-cities_size = os.path.getsize("examples/cities.csv")
-sample_size = os.path.getsize("examples/sample.pdf")
-sample3_size = os.path.getsize("examples/sample3.xlsx")
+def check_row_num_in_csv():
+    with open("examples/cities.csv", 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        csv_rows = 0
+        for row in csv_reader:
+            csv_rows += 1
+        return csv_rows
 
 
-def test_check_files_in_archive():
+def check_page_num_in_pdf():
+    with open("examples/sample.pdf", "rb") as pdffile:
+        pdf_reader = PyPDF2.PdfReader(pdffile)
+        pdf_numpages = len(pdf_reader.pages)
+        return pdf_numpages
+
+
+def move_files_into_archive():
     with ZipFile("resourses/myzip.zip", "w", compression=ZIP_DEFLATED, compresslevel=5) as myzip:
         myzip.write("examples/cities.csv", "cities.csv")
         cities_size_in_archive = myzip.getinfo("cities.csv").file_size
@@ -43,12 +47,22 @@ def test_check_files_in_archive():
         sheet = xlsx_reader.active
         xsls_rows_in_archive = sheet.max_row
         sample3_size_in_archive = myzip.getinfo("sample3.xlsx").file_size
-        assert myzip.testzip() is None
+        return [cities_size_in_archive, pdf_numpages_in_archive, sample_size_in_archive, \
+                xsls_rows_in_archive, sample3_size_in_archive, csv_rows_in_archive]
 
 
-    assert cities_size == cities_size_in_archive, f"Размер файла в архиве не совпадает с тем, что был до архивирования"
-    assert sample_size == sample_size_in_archive, f"Размер файла в архиве не совпадает с тем, что был до архивирования"
-    assert sample3_size == sample3_size_in_archive, f"Размер файла в архиве не совпадает с тем, что был до архивирования"
-    assert csv_rows == csv_rows_in_archive, f"Количество строк csv файла в архиве не совпадает с изначальным кол-вом"
-    assert pdf_numpages == pdf_numpages_in_archive, f"Количество страниц pdf файла в архиве не совпадает с изначальным кол-вом"
-    assert xlsx_rows == xsls_rows_in_archive, f"Количество строк на активном листе файла xlsx в архиве не совпадает с изначальным кол-вом"
+cities_size = os.path.getsize("examples/cities.csv")
+sample_size = os.path.getsize("examples/sample.pdf")
+sample3_size = os.path.getsize("examples/sample3.xlsx")
+
+
+def test_check_files_in_archive():
+    rez = move_files_into_archive()
+    assert cities_size == rez[0], f"Размер файла в архиве не совпадает с тем, что был до архивирования"
+    assert sample_size == rez[2], f"Размер файла в архиве не совпадает с тем, что был до архивирования"
+    assert sample3_size == rez[4], f"Размер файла в архиве не совпадает с тем, что был до архивирования"
+    assert check_row_num_in_csv() == rez[5], f"Количество строк csv файла в архиве не совпадает с изначальным кол-вом"
+    assert check_page_num_in_pdf() == rez[1], f"Количество страниц pdf файла в архиве не совпадает с изначальным кол-вом"
+    assert check_row_num_in_xlsx() == rez[3], f"Количество строк на активном листе файла xlsx в архиве не совпадает с изначальным кол-вом"
+
+
